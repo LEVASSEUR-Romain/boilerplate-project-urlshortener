dbe4b7029ccf41806f586440956f690c9a6e0b02
Romain <romainlev@gmail.com>
2022-07-05 00:47:37 +0200

    freedcode camp exo3

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
+// Exercice
+let short = 1;
+let listUrl = [];
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+const jsonParser = bodyParser.json();
+const handler = (req, res) => {
+  // regex to update
+  if (req.body.url.match(/https:\/\//)) {
+    res.send({ original_url: req.body.url, short_url: short });
+    short++;
+    listUrl.push(req.body.url);
+  } else res.send({ error: "invalid url" });
+};
+app.post("/api/shorturl", jsonParser, handler);
+
+app.get("/api/shorturl/:text", (req, res) => {
+  const getResult = parseInt(req.params.text);
+  res.redirect(listUrl[getResult - 1]);
+});
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
